14ef51143052801337fd49b707d8ee524ea988fe
schildner <eduard.schildner@gmail.com>
2021-01-23 17:36:09 +0100

    Remove count mechanism for variable number of vms.

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,6 +1,8 @@
 resource "azurerm_network_interface" "test" {
-  count               = var.number_of_vms
-  name                = "nic-${count.index}"
+  #count               = var.number_of_vms
+  #name                = "nic-${count.index}"
+  
+  name                = "nic0"
   location            = var.location
   resource_group_name = var.resource_group
 
@@ -12,52 +14,39 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "test" {
-  name                = "ubuntu1604vm"
-  count               = var.number_of_vms
-  location            = var.location
-  resource_group_name = var.resource_group
-  size                = "Standard_B1s"
-  admin_username      = var.username
+resource "azurerm_linux_virtual_machine" "main" {
+  #count                           = var.number_of_vms
+  #name                            = "vm-${count.index}"
+  
+  name                            = "vm0"
+  resource_group_name             = var.resource_group
+  location                        = var.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.username
+  #admin_password                  = var.password
+  #source_image_id                 = var.packer_image
+  #disable_password_authentication = false
   
   network_interface_ids = [
-    element(azurerm_network_interface.test.*.id, count.index)
+    #element(azurerm_network_interface.test.*.id, count.index)
+    azurerm_network_interface.test.id
   ]
+
   admin_ssh_key {
     username   = "eduard"
     public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
-    caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
   }
-  
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
-  }
-}
-resource "azurerm_linux_virtual_machine" "main" {
-  count                           = var.number_of_vms
-  name                            = "vm-${count.index}"
-  resource_group_name             = var.resource_group
-  location                        = var.location
-  size                            = "Standard_B1s"
-  admin_username                  = var.username
-  admin_password                  = var.password
-  #source_image_id                 = var.packer_image
-  disable_password_authentication = false
-  
-  network_interface_ids = [
-    element(azurerm_network_interface.test.*.id, count.index)
-  ]
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
   }
 
   tags = {
@@ -65,3 +54,32 @@ resource "azurerm_linux_virtual_machine" "main" {
     stage        = "Submission"
   }
 }
+
+#resource "azurerm_linux_virtual_machine" "test" {
+#  name                = "ubuntu1604vm"
+#  count               = var.number_of_vms
+#  location            = var.location
+#  resource_group_name = var.resource_group
+#  size                = "Standard_B1s"
+#  admin_username      = var.username
+#  
+#  network_interface_ids = [
+#    element(azurerm_network_interface.test.*.id, count.index)
+#  ]
+#  admin_ssh_key {
+#    username   = "eduard"
+#    public_key = file("~/.ssh/id_rsa.pub")
+#  }
+#
+#  os_disk {
+#    caching           = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
+#
+#  source_image_reference {
+#    publisher = "Canonical"
+#    offer     = "UbuntuServer"
+#    sku       = "16.04-LTS"
+#    version   = "latest"
+#  }
+#}
